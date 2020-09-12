02b7c0638c0ee2f8616085146662e3421da9f309
Thanh Long <longht021189@gmail.com>
2020-09-12 12:28:06 +0700

    add github token

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const masterName = core.getInput('master-name');
-  const expiredTime = core.getInput('expired-time');
-  console.log(`Hello ${nameToGreet} ${expiredTime}!`);
+  const masterName = core.getInput('master-name', { required: true });
+  const expiredTime = core.getInput('expired-time', { required: true });
+  const githubToken = core.getInput("github-token", { required: true });
+
+  const client = github.getOctokit(github_token);
+
+  console.log(`Hello ${masterName} ${expiredTime} ${githubToken}!`);
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
