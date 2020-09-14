6ca9c00c0feae68b270ce40070ec58dad584b33e
Thanh Long <longht021189@gmail.com>
2020-09-14 09:15:56 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const masterName = core.getInput('master-name', { required: true });
-  const expiredTime = core.getInput('expired-time', { required: true });
-  const githubToken = core.getInput("github-token", { required: true });
-
-  const client = github.getOctokit(github_token);
-
-  console.log(`Hello ${masterName} ${expiredTime} ${githubToken}!`);
-
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const masterName = core.getInput('master-name', { required: true });
+    const expiredTime = core.getInput('expired-time', { required: true });
+    const githubToken = core.getInput("github-token", { required: true });
+  
+    const client = github.getOctokit(github_token);
+    const repo = github.context.repo;
+    const list = await client.repos.listBranches()
+  
+    console.log(`Hello ${masterName}, expiredTime = ${expiredTime}, githubToken = ${githubToken}, repo = ${repo}, list = ${list}!`);
+  
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+  
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
