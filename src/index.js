780df0c8e1868a677ff4683b533fdf3272bf045a
Thanh Long <longht021189@gmail.com>
2020-09-16 09:20:17 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,23 +4,16 @@ const github = require('@actions/github');
 async function run() {
   const masterName = core.getInput('master-name', { required: true });
   const expiredTime = core.getInput('expired-time', { required: true });
+  const githubToken = core.getInput('github-token', { required: true });
+  const client = github.getOctokit(githubToken);
+  const list = await client.repos.listBranches()
 
-  console.log(`masterName = ${masterName}, expiredTime = ${expiredTime}`);
-
-  // try {
-  //   
-  //   
-  //   const githubToken = core.getInput('github-token', { required: true });
+  console.log(`masterName = ${masterName}, expiredTime = ${expiredTime}, githubToken = ${githubToken}, list = ${list}`);
   
-  //   const client = github.getOctokit(githubToken);
   //   const repo = github.context.repo;
-  //   const list = await client.repos.listBranches()
-  
-  //   console.log(`Hello , githubToken = ${githubToken}, repo = ${repo}, list = ${list}!`);
-  
+  //   console.log(`Hello , !`);
   //   // const time = (new Date()).toTimeString();
   //   // core.setOutput("time", time);
-  
   //   const payload = JSON.stringify(github.context.payload, undefined, 2)
   //   console.log(`The event payload: ${payload}`);
   // } catch (error) {
