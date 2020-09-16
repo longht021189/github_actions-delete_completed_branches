ddacbc4e0a897ca1913bf77029899d8b6e8a9321
Thanh Long <longht021189@gmail.com>
2020-09-16 09:09:49 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const masterName = core.getInput('master-name', { required: true });
+    const expiredTime = core.getInput('expired-time', { required: true });
+    const githubToken = core.getInput('github-token', { required: true });
+  
+    const client = github.getOctokit(githubToken);
+    const repo = github.context.repo;
+    const list = await client.repos.listBranches()
+  
+    console.log(`Hello ${masterName}, expiredTime = ${expiredTime}, githubToken = ${githubToken}, repo = ${repo}, list = ${list}!`);
+  
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+  
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
